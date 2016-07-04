7f991f6e27a28b8ddd322777dc544a9233e1c00d
Tyler Wong <tbwong3@gmail.com>
2016-07-03 17:22:08 -0700

    redid splash screen

--- app/src/main/java/com/app/pokebase/pokebase/activities/SplashActivity.java
+++ app/src/main/java/com/app/pokebase/pokebase/activities/SplashActivity.java
@@ -4,69 +4,67 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.app.pokebase.pokebase.R;
+import com.app.pokebase.pokebase.database.DatabaseOpenHelper;
 import com.app.pokebase.pokebase.utilities.Typefaces;
 
 /**
  * @author Brittany Berlanga
  */
 public class SplashActivity extends AppCompatActivity {
+
    private TextView mTitleLabel;
+
    private Typeface mRobotoLight;
 
-   final static int SPLASH_DISPLAY_LENGTH = 1000;
    final static String ROBOTO_PATH = "fonts/roboto-light.ttf";
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       mRobotoLight = Typefaces.get(this, ROBOTO_PATH);
-
       setContentView(R.layout.activity_splash);
 
       mTitleLabel = (TextView) findViewById(R.id.title_label);
+      mTitleLabel.setTypeface(mRobotoLight);
 
-      if (mRobotoLight != null) {
-         mTitleLabel.setTypeface(mRobotoLight);
-      }
-
-      SharedPreferences pref = getSharedPreferences("ActivityPREF", Context.MODE_PRIVATE);
-      boolean appIntroFinished = pref.getBoolean("appIntroFinished", false);
-      boolean signUpComplete = pref.getBoolean("signUpComplete", false);
-
-      if (appIntroFinished && signUpComplete) {
-         Intent mainIntent = new Intent(this, MainActivity.class);
-         startActivity(mainIntent);
-         finish();
-      }
-      else if (appIntroFinished && !signUpComplete) {
-         Intent signUpIntent = new Intent(this, SignUpActivity.class);
-         startActivity(signUpIntent);
-         finish();
-      }
+      new DatabaseTask().execute();
    }
 
-   @Override
-   protected void onResume() {
-      super.onResume();
+   private class DatabaseTask extends AsyncTask<Void, Void, Void> {
+      @Override
+      protected Void doInBackground(Void... voids) {
+         DatabaseOpenHelper database = DatabaseOpenHelper.getInstance(SplashActivity.this);
+         return null;
+      }
 
-      new Handler().postDelayed(new Runnable() {
-         @Override
-         public void run() {
-            Intent startIntent = new Intent(SplashActivity.this, StartActivity.class);
-            startIntent.putExtra("logo_transition", true);
-            LinearLayout logo = (LinearLayout) findViewById(R.id.logo);
-            ActivityOptionsCompat options = ActivityOptionsCompat.
-                  makeSceneTransitionAnimation(SplashActivity.this, logo, getString(R.string.logo_transition));
-            SplashActivity.this.startActivity(startIntent, options.toBundle());
+      @Override
+      protected void onPostExecute(Void aVoid) {
+         super.onPostExecute(aVoid);
+
+         SharedPreferences pref = getSharedPreferences("ActivityPREF", Context.MODE_PRIVATE);
+         boolean appIntroFinished = pref.getBoolean("appIntroFinished", false);
+         boolean signUpComplete = pref.getBoolean("signUpComplete", false);
+
+         Intent resultIntent;
+
+         if (appIntroFinished && signUpComplete) {
+            resultIntent = new Intent(SplashActivity.this, MainActivity.class);
          }
-      }, SPLASH_DISPLAY_LENGTH);
+         else if (appIntroFinished && !signUpComplete) {
+            resultIntent = new Intent(SplashActivity.this, SignUpActivity.class);
+         }
+         else {
+            resultIntent = new Intent(SplashActivity.this, IntroActivity.class);
+         }
+
+         startActivity(resultIntent);
+         finish();
+      }
    }
 }
