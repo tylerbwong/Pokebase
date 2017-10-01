138dcaf867ddd7eff1adeaf62d6cbde4444d41e9
Tyler Wong <tbwong3@gmail.com>
2017-10-01 01:38:47 -0700

    Remove import

--- app/src/main/java/me/tylerbwong/pokebase/gui/activities/PokemonProfileActivity.java
+++ app/src/main/java/me/tylerbwong/pokebase/gui/activities/PokemonProfileActivity.java
@@ -29,7 +29,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Pair;
-import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
